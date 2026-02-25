8d0a39c1dbc62084fd93f2a4d09e28307d7fc675
YakshPatel0805 <yakshpatel6464@gmail.com>
2026-02-25 14:59:56 +0530

    updation

--- src/pages/AdminProductsView.tsx
+++ src/pages/AdminProductsView.tsx
@@ -434,10 +434,9 @@ export default function AdminProductsView() {
                         Reviews
                       </label>
                       <input
-                        type="number"
-                        min="0"
+                        type="string"
                         value={editingProduct.reviews}
-                        onChange={(e) => setEditingProduct({ ...editingProduct, reviews: parseInt(e.target.value) })}
+                        onChange={(e) => setEditingProduct({ ...editingProduct, reviews: e.target.value })}
                         className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500"
                         required
                       />
